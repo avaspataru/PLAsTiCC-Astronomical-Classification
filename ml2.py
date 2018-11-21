ba251f9162c707620e9cb9be3fb3f9182fc85e46
Ava Spataru <A.Spataru@warwick.ac.uk>
2018-11-21 21:09:14 +0000

    formatted code

--- ml2.py
+++ ml2.py
@@ -19,6 +19,73 @@ def scale(dataFrame):
     gc.collect()
     return ss.fit_transform(dataFrame_new)
 
+
+def splitGalaxies(dataFrame, targets):
+    print "Split extragalactic "
+    extra = np.where(dataFrame['hostgal_photoz']==0.0)
+    extragalactic_data = dataFrame.drop(dataFrame.index[extra])
+    extra_ids = extragalactic_data['object_id']
+    extragalactic_data.drop('object_id',axis=1)
+    extragalactic_targets = targets.drop(targets.index[extra])
+
+    extragalactic_data = scale(extragalactic_data)
+
+    print "Split intragalactic "
+    intra = np.where(dataFrame['hostgal_photoz']!=0.0)
+    intragalactic_data = dataFrame.drop(dataFrame.index[intra])
+    intra_ids = intragalactic_data['object_id']
+    intragalactic_data.drop('object_id',axis=1)
+    intragalactic_targets = targets.drop(targets.index[intra])
+
+    intragalactic_data = scale(intragalactic_data)
+    #return extragalactic_data, extragalactic_targets, extra_ids, intragalactic_data, intragalactic_targets, intra_ids
+    return extragalactic_data, extragalactic_targets,extra_ids, intragalactic_data, intragalactic_targets, intra_ids
+
+
+def format(set_metadata_raw, set_raw):
+    print "Formatting data"
+    set_data = set_metadata_raw.drop('target',axis=1)
+    set_data = set_data.drop('distmod',axis=1)
+
+    set_raw['flux_ratio_sq'] = np.power(set_raw['flux'] / set_raw['flux_err'], 2.0)
+    set_raw['flux_by_flux_ratio_sq'] = set_raw['flux'] * set_raw['flux_ratio_sq']
+
+    aggs = {
+    'mjd': ['min', 'max', 'size'],
+    'passband': ['min', 'max', 'mean', 'median', 'std'],
+    'flux': ['min', 'max', 'mean', 'median', 'std','skew'],
+    'flux_err': ['min', 'max', 'mean', 'median', 'std','skew'],
+    'detected': ['mean'],
+    'flux_ratio_sq':['sum','skew'],
+    'flux_by_flux_ratio_sq':['sum','skew'],
+    }
+
+    agg_train = set_raw.groupby('object_id').agg(aggs)
+
+    new_columns = [
+    k + '_' + agg for k in aggs.keys() for agg in aggs[k]
+    ]
+    agg_train.columns = new_columns
+    agg_train['mjd_diff'] = agg_train['mjd_max'] - agg_train['mjd_min']
+    agg_train['flux_diff'] = agg_train['flux_max'] - agg_train['flux_min']
+    agg_train['flux_dif2'] = (agg_train['flux_max'] - agg_train['flux_min']) / agg_train['flux_mean']
+    agg_train['flux_w_mean'] = agg_train['flux_by_flux_ratio_sq_sum'] / agg_train['flux_ratio_sq_sum']
+    agg_train['flux_dif3'] = (agg_train['flux_max'] - agg_train['flux_min']) / agg_train['flux_w_mean']
+
+    del agg_train['mjd_max'], agg_train['mjd_min']
+    agg_train.head()
+
+    del set_raw
+    gc.collect()
+
+    full_train = agg_train.reset_index().merge(
+    right=set_data, # this is without some cols
+    how='outer',
+    on='object_id'
+    )
+
+    return full_train
+
 def main():
 
     print "Reading data"
@@ -34,73 +101,20 @@ def main():
     for c in [64, 15]:
         class_weight[c] = 2
 
-    print "Formatting data"
-    training_set_data = training_set_metadata_raw.drop('target',axis=1)
-    training_set_data = training_set_data.drop('distmod',axis=1)
 
-    training_set_raw['flux_ratio_sq'] = np.power(training_set_raw['flux'] / training_set_raw['flux_err'], 2.0)
-    training_set_raw['flux_by_flux_ratio_sq'] = training_set_raw['flux'] * training_set_raw['flux_ratio_sq']
+    full_train = format(training_set_metadata_raw, training_set_raw)
+    extragalactic_data, extragalactic_targets, extra_ids,  intragalactic_data, intragalactic_targets, intra_ids = splitGalaxies(full_train, training_set_targets)
 
-    aggs = {
-    'mjd': ['min', 'max', 'size'],
-    'passband': ['min', 'max', 'mean', 'median', 'std'],
-    'flux': ['min', 'max', 'mean', 'median', 'std','skew'],
-    'flux_err': ['min', 'max', 'mean', 'median', 'std','skew'],
-    'detected': ['mean'],
-    'flux_ratio_sq':['sum','skew'],
-    'flux_by_flux_ratio_sq':['sum','skew'],
-    }
-
-    agg_train = training_set_raw.groupby('object_id').agg(aggs)
-
-    new_columns = [
-    k + '_' + agg for k in aggs.keys() for agg in aggs[k]
-    ]
-    agg_train.columns = new_columns
-    agg_train['mjd_diff'] = agg_train['mjd_max'] - agg_train['mjd_min']
-    agg_train['flux_diff'] = agg_train['flux_max'] - agg_train['flux_min']
-    agg_train['flux_dif2'] = (agg_train['flux_max'] - agg_train['flux_min']) / agg_train['flux_mean']
-    agg_train['flux_w_mean'] = agg_train['flux_by_flux_ratio_sq_sum'] / agg_train['flux_ratio_sq_sum']
-    agg_train['flux_dif3'] = (agg_train['flux_max'] - agg_train['flux_min']) / agg_train['flux_w_mean']
-
-    del agg_train['mjd_max'], agg_train['mjd_min']
-    agg_train.head()
-
-    del training_set_raw
-    gc.collect()
-
-    full_train = agg_train.reset_index().merge(
-    right=training_set_data, # this is without some cols
-    how='outer',
-    on='object_id'
-    )
-
-    full_train_ids = full_train['object_id']
-    full_train = full_train.drop('object_id', axis=1)
-
-    print full_train.columns
-
-    print "Split extragalactic "
-    extra = np.where(full_train['hostgal_photoz']==0.0)
-    extragalactic_data = full_train.drop(full_train.index[extra])
-    extragalactic_targets = training_set_targets.drop(training_set_targets.index[extra])
-
-    extragalactic_data = scale(extragalactic_data)
     print "Model for extra:"
     clf = RandomForestClassifier(n_jobs=2, random_state=0)
     print cross_val_score(clf, extragalactic_data, extragalactic_targets, cv=10, scoring="neg_log_loss").mean()
 
-    print "Split intragalactic "
-    intra = np.where(full_train['hostgal_photoz']!=0.0)
-    intragalactic_data = full_train.drop(full_train.index[intra])
-    intragalactic_targets = training_set_targets.drop(training_set_targets.index[intra])
-
-    intragalactic_data = scale(intragalactic_data)
     print "Model for intra:"
     clf = RandomForestClassifier(n_jobs=2, random_state=0)
     print cross_val_score(clf, intragalactic_data, intragalactic_targets, cv=10, scoring="neg_log_loss").mean()
 
-
+    full_train_ids = full_train['object_id']
+    full_train = full_train.drop('object_id', axis=1)
     full_train = scale(full_train)
     print "Training model"
     clf = RandomForestClassifier(n_jobs=2, random_state=0)
