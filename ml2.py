60f81219de86805a576aa26b6fd42253e015e084
Ava Spataru <A.Spataru@warwick.ac.uk>
2018-11-23 01:07:44 +0000

    outputs smth

--- ml2.py
+++ ml2.py
@@ -54,7 +54,6 @@ def splitGalaxies(dataFrame, targets):
 
 
 def format(set_metadata_raw, set_raw):
-    print "Formatting data"
 
     set_data = set_metadata_raw.drop('distmod',axis=1)
 
@@ -165,67 +164,6 @@ def fill_in_hostgal_specz(dataFrame):
     df['hostgal_specz'] = df['hostgal_photoz']
     return df
 
-def do_my_predict(extra_model, intra_model):
-    print "some other stuff"
-    print "Reading test data"
-    test_set_metadata_raw = pd.read_csv('/modules/cs342/Assignment2/test_set_metadata.csv')
-    path = '/modules/cs342/Assignment2/test_set.csv'
-
-     # set initial state
-    last_id = None
-    last_df = pd.DataFrame()
-
-    for df in pd.read_csv(path, chunksize=chunksize):
-        # Group by object_id; store grouped dataframes into dict for fast access
-        grouper = {
-            object_id: pd.DataFrame(group)
-            for object_id, group in df.groupby('object_id')
-        }
-
-        # queue unique object_ids, in order, for processing
-        object_ids = df['object_id'].unique()
-        queue = deque(object_ids)
-
-        # if the object carried over from previous chunk matches
-        # the first object in this chunk, stitch them together
-        first_id = queue[0]
-        if first_id == last_id:
-            first_df = grouper[first_id]
-            last_df = pd.concat([last_df, first_df])
-            grouper[first_id] = last_df
-        elif last_id is not None:
-            # save last_df and return as first result
-            grouper[last_id] = last_df
-            queue.appendleft(last_id)
-
-        # save last object in chunk
-        last_id = queue[-1]
-        last_df = grouper[last_id]
-
-        # check for edge case with only one object_id in this chunk
-        if first_id == last_id:
-            # yield nothing for now...
-            continue
-         # yield all but last object, which may be incomplete in this chunk
-        while len(queue) > 1:
-            object_id = queue.popleft()
-            object_df = grouper.pop(object_id)
-            yield (object_id, object_df)
-
-     # yield remaining object
-    yield (last_id, last_df)
-
-
-    #print "Formatting test data"
-    #test_set_targets = test_set_metadata_raw['target']
-
-    #full_test = format(test_set_metadata_raw, test_set_raw)
-    #extragalactic_test_data, extragalactic_test_targets, extra_test_ids,  intragalactic_test_data, intragalactic_test_targets, intra_test_ids = splitGalaxies(full_test, test_set_targets)
-
-    #ans = bcf.predict_proba(test_set.values)[:, 1]
-    #df = pd.DataFrame(ans)
-    #df.to_csv("test_set_predictions.csv", index = False, header = False)
-
 def main():
 
     mode = 1 #0-cv, 1-predict
@@ -281,18 +219,140 @@ def main():
         test_set_metadata_raw = pd.read_csv('/modules/cs342/Assignment2/test_set_metadata.csv')
         filepath = '/modules/cs342/Assignment2/test_set.csv'
 
-        for object_id, df in get_objects_by_id(filepath):
-            combined = format(fill_in_hostgal_specz(test_set_metadata_raw.loc[test_set_metadata_raw['object_id']==object_id]),df)
-            combined = combined.drop('object_id',axis=1)
-            combined = scale(combined)
+        extra_classes = extra_model.classes_
+        intra_classes = intra_model.classes_
+        column_names = []
+        column_names.append('object_id')
+        for classi in extra_classes:
+            className = "class_" + str(classi)
+            column_names.append(className)
+        for classi in intra_classes:
+            className = "class_" + str(classi)
+            column_names.append(className)
+        column_names.append("class_99")
+        lst = []
+        count = 0
+        batch_no = 0
+        batch_list = []
+        print " >Starting new batch 0"
+        for obj_id, d in get_objects_by_id(filepath):
+            batch_list.append((obj_id,d))
+            if(count == 2):
+                print " >>Formatting batch objects"
+                batch_extra_dataFrame = pd.DataFrame()
+                batch_intra_dataFrame = pd.DataFrame()
+                myextrabatchlist = []
+                myintrabatchlist = []
+                for object_id, df in batch_list:
+                    combined = format(fill_in_hostgal_specz(test_set_metadata_raw.loc[test_set_metadata_raw['object_id']==object_id]),df)
+                    #combined = combined.drop('object_id',axis=1)
+                    combined = scale(combined) #combined is a df with one row for this object id
+                    if (object_id in extra_ids):
+                        myextrabatchlist.append(combined.values[0])
+                    else:
+                        myintrabatchlist.append(combined.values[0])
+                batch_extra_dataFrame= pd.DataFrame(myextrabatchlist,columns=combined.columns)
+                batch_intra_dataFrame= pd.DataFrame(myintrabatchlist,columns=combined.columns)
+                extra_ans = []
+                intra_ans = []
+                objids = [[]]
+                print " >>Predicting extra"
+                if(len(batch_extra_dataFrame.index)>0):
+                    objids1 = batch_extra_dataFrame['object_id'].values.tolist()
 
-            if object_id in extra_ids:
-                print str(object_id) + " is extra"
-                ans = extra_model.predict_proba(combined)[:,1]
+                    objids = []
+                    for id in objids1:
+                        l1 = [id]
+                        print l1
+                        objids.append(l1)
+                        print objids
+                    batch_extra_dataFrame = batch_extra_dataFrame.drop('object_id', axis=1)
+                    extra_ans = extra_model.predict_proba(batch_extra_dataFrame)
+                    z = np.zeros((len(extra_ans),6)) # zeros for intra classes and class 99
+                    print extra_ans
+                    print z
+                    extra_ans = np.append(extra_ans,z,axis=1)
+                    print extra_ans
+                    print objids
+                    extra_ans = np.append(objids,extra_ans,axis=1)
+                print " >>Predicting intra"
+                objids = [[]]
+                if(len(batch_intra_dataFrame.index)>0):
+                    objids1 = batch_intra_dataFrame['object_id'].values.tolist()
+
+                    objids = []
+                    for id in objids1:
+                        l1 = [id]
+                        print l1
+                        objids.append(l1)
+                        print objids
+                    batch_intra_dataFrame = batch_intra_dataFrame.drop('object_id', axis=1)
+                    intra_ans = intra_model.predict_proba(batch_intra_dataFrame)
+                    z = np.zeros((len(intra_ans),10)) # zeros for extra classes and class 99
+                    intra_ans = np.append(z,intra_ans,axis=1)
+                    intra_ans = np.append(objids,intra_ans,axis=1)
+
+                print extra_ans
+                print intra_ans
+                print " >>Putting together"
+                arr = []
+                if((len(batch_extra_dataFrame.index)>0) and (len(batch_intra_dataFrame.index)>0) ):
+                    arr = np.concatenate((extra_ans,intra_ans), axis=0)
+                else:
+                    if (len(batch_intra_dataFrame.index)>0):
+                        arr = intra_ans
+                    else:
+                        if (len(batch_extra_dataFrame.index)>0):
+                            arr = extra_ans
+
+                print " >>Write to csv"
+                finish = pd.DataFrame(arr, columns=column_names)
+                print finish
+                print finish
+                if(batch_no==0):
+                    finish.to_csv("predictions.csv", index = False, header = True)
+                else:
+                    with open('predictions.csv', 'a') as f:
+                        finish.to_csv(f, index = False, header=False)
+                break
+
+                #index = 0
+                #row= []
+                #row.append(object_id)
+                #if object_id in extra_ids:
+                #    ans = extra_model.predict_proba(combined)
+                #    for classi in extra_classes:
+                #        row.append(ans[:,index][0])
+                #        index = index + 1
+                #    for classi in intra_classes:
+                #        row.append(0.0)
+
+                #else:
+                #    ans = intra_model.predict_proba(combined)
+                #    for classi in extra_classes:
+                #        row.append(0.0)
+                #    for classi in intra_classes:
+                #        row.append(ans[:,index][0])
+                #        index = index + 1
+                #row.append(0.0) #class99
+                #lst.append(row)
+
+                print " >Starting new batch" + str(batch_no + 1)
+                batch_no = batch_no + 1
+                lst = 0
+                count = 0
+                finish = pd.DataFrame(columns=column_names)
+
             else:
-                print str(object_id) + " is intra"
-                ans = intra_model.predict_proba(combined)[:,1]
-            print ans
+                count = count + 1
+                if(count == 2500):
+                    print "  >>25%..."
+                if(count == 5000):
+                    print "  >>50%..."
+                if(count == 7500):
+                    print "  >>75%..."
+            if(batch_no == 3):
+                break
         print "DONE."
 
 main()
